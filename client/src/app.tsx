ccd9c302d622b0d9cd7fc59cea04e750befec3cd
Adrian Gomez <aegomez@tekmexico.com>
2019-09-30 15:54:04 -0700

    Set up React Router in App.tsx

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Navbar from './components/layout/navbar';
 import Landing from './components/layout/landing';
+import Register from './components/auth/register';
+import Login from './components/auth/login';
 
 class App extends React.Component {
   render() {
     return (
-      <div className='App'>
-        <Navbar />
-        <Landing />
-      </div>
+      <BrowserRouter>
+        <div className='App'>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
